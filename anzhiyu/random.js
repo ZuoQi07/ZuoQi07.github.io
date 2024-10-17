a12c98b4f7dafed30e29a3071b32224c59e2becc
ZuoQi07 <1701777353@qq.com>
2024-10-17 17:25:59 +0800

    Site updated: 2024-10-17 17:25:58

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/02/03/STM-DIY项目宣传文稿/","2024/10/15/关于”PL2303HXA自2012已停产-请联系供货商”/","2024/10/15/对安知鱼主题颜色设置的补充/"];function toRandomPost(){
+var posts=["2024/02/03/STM-DIY项目宣传文稿/","2024/10/15/对安知鱼主题颜色设置的补充/","2024/10/15/关于”PL2303HXA自2012已停产-请联系供货商”/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
